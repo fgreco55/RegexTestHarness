d92561ecb947669ef0e7410db2a14be25cdfbd55
Frank Greco <fgreco@javasig.com>
2022-02-27 21:18:50 -0500

    regex tester - borrowed from Oracle's regex tester.  I converted to use Scanner

---
+++ src/main/java/RegexTestHarness.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class RegexTestHarness {
+
+    public static void main(String[] args){
+        Scanner console;
+      
+        while (true) {
+            console = new Scanner(System.in);
+
+            System.out.print("Enter regex: ");
+            Pattern pattern = Pattern.compile(console.nextLine());
+
+            System.out.print("Enter input string to search: ");
+            Matcher matcher = pattern.matcher(console.nextLine());
+
+            boolean found = false;
+            while ( matcher.find() ) {
+                if (matcher.group() == "")
+                    break;
+                System.out.printf("I found the text" +
+                    " \"%s\" starting at " +
+                    "index %d and ending at index %d.%n",
+                    matcher.group(),
+                    matcher.start(),
+                    matcher.end());
+                found = true;
+            }
+            if( !found ) {
+                System.out.printf("No match found.%n");
+            }
+        }
+    }
+}
